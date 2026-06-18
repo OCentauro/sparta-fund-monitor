968c4271e05162eb08ecfb395a4dd4eb24e74c28
Centauro <ejosivan@gmail.com>
2026-06-18 10:33:38 -0300

    feat: add PWA support

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'sparta-fund-monitor-v1';
+const urlsToCache = [
+  '/sparta-fund-monitor/',
+  '/sparta-fund-monitor/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
